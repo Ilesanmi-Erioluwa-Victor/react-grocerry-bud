2f6c2034d67fc6a2c9b519426d7aedaa4eb97f2b
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-03 16:54:25 +0100

    Added a few hooks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
   return (
     <div className="App">
       <h3>Hello</h3>
