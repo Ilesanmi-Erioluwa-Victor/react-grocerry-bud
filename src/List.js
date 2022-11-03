6927a03c17a34fa64134d2dd38b585a74fbbf899
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-03 17:25:20 +0100

    Started working on list

--- src/List.js
+++ src/List.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
-export const List = () => {
-  return <div>List</div>;
+export const List = ({ item }) => {
+  return (
+    <div className="grocery-list">
+      {item.map((item) => {
+        const { id, newItem } = item;
+        return <article key={id}></article>;
+      })}
+    </div>
+  );
 };
