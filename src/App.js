14473be59ba4e0d6011f13df81f31293a01a1d83
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-03 16:47:37 +0100

    Added more states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <div className="App">
       <h3>Hello</h3>
