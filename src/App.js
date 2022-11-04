25fea63a6c843ceb98ce329c9ca5e11733534ddb
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-04 11:05:31 +0100

    Final push

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useState, useEffect } from "react";
 import { Alert } from "./Alert";
 import { List } from "./List";
 
+const getListFromLocalStorage = () => {
+  let getList = localStorage.getItem("list");
+  if (getList) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  // When dealing with local storage, always return as default value in your hook
+  const [list, setList] = useState(getListFromLocalStorage);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({
